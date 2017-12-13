a67d6e040fb53f46d9d260a6f8aa78fe1f3d15e0
TheOnlyArtz <gamesil456@gmail.com>
2017-12-13 18:11:31 +0200

    The fix for the never end activity!

--- src/send-discord.js
+++ src/send-discord.js
@@ -1,30 +1,8 @@
 const {ipcMain} = require('electron');
 const path = require('path');
-const snekfetch = require("snekfetch")
-const startTimestamp = new Date().getTime() / 1000;
-
-let projectName = null;
-let currEditor = null;
-let largeImage = null;
-
-let translations = {};
-let privacySettings = {};
-let behaviour = {};
-
-let matchData;
-let matchKeys = [];
-let rpc;
-
-const REGEX_REGEX = /^\/(.*)\/([mgiy]+)$/;
-
-const getTranslation = (key, args = {}) => {
-	let tr = translations[key];
-	if(!tr) return "UNDEFINED_TRANSLATIONS";
-
-	Object.keys(args).forEach((i) => tr = tr.replace(new RegExp(`%${i}%`, 'g'), args[i]));
-
-	return tr;
-};
+const snekfetch = require("snekfetch");
+let Client = undefined;
+const DISCORD_ID = '380510159094546443';
 
 if (!String.prototype.padStart) {
 	String.prototype.padStart = function padStart(targetLength,padString) {
@@ -43,105 +21,255 @@ if (!String.prototype.padStart) {
 	};
 }
 
-const setupRpc = (Client) => {
-	rpc = new Client({ transport: 'ipc' });
+// Setup configuration object
+const config = {
+	translations: {},
+	privacy: {},
+	behaviour: {},
+	getTranslation(key, args = {}) {
+		let tr = config.translations[key];
+		if(!tr) return "UNDEFINED_TRANSLATION";
 
-	rpc.on('ready', () => {
-		const loop = () => {
-			let state = projectName ? getTranslation('working-project', {
-				projectName
-			}) : getTranslation('working-no-project');
+		Object.keys(args).forEach((i) => tr = tr.replace(new RegExp(`%${i}%`, 'g'), args[i]));
 
-			let details = currEditor ? getTranslation('editing-file', {
-				fileName: currEditor
-			}) : getTranslation('editing-idle');
-
-			let largeImageInner = largeImage;
-
-			let smallImageKey = 'atom';
-
-
-			if(!privacySettings.sendProject) state = getTranslation('working-no-project');
-			if(!privacySettings.sendFilename) details = getTranslation('type-unknown');
-			if(!privacySettings.sendFileType) largeImageInner = {
-					icon: 'text',
-					text: getTranslation('type-unknown')
-				};
-
-			const packet = {
-				state,
-				details,
-				largeImageKey: largeImageInner.icon,
-				largeImageText: largeImageInner.text
-			};
-
-			if(privacySettings.sendElapsed) packet.startTimestamp = startTimestamp;
-
-			if(behaviour.smallIconToggle) {
-				packet.smallImageKey = smallImageKey;
-				packet.smallImageText = getTranslation('atom-description')
-			}
-
-			rpc.setActivity(packet);
-
-			setTimeout(loop, behaviour.updateTick);
-		};
-
-		loop();
-	});
+		return tr;
+	},
+	icon: new Map()
 };
 
-ipcMain.on('atom-discord.data-update', (event, arg) => {
-	projectName = arg.projectName;
-	currEditor = arg.currEditor;
+const normalize = (object) => {
+	Object.keys(object).forEach((k) => {
+		if(object[k] === null){
+			delete object[k];
+		}
+	});
 
-	if(currEditor && matchKeys) {
-		largeImage = matchData[matchKeys.find((k) => {
-			if(k.startsWith('.') && currEditor.endsWith(k)) return true;
+	return object;
+};
 
-			let match = k.match(REGEX_REGEX);
-			if(!match) return false;
+class DiscordSender {
+	constructor() {
+		this.projectName = null;
+		this.fileName = null;
+		this.largeImage = null;
+		this.startTimestamp = new Date().getTime() / 1000;
 
-			const regex = new RegExp(match[1], match[2]);
-			return regex.test(currEditor);
-		})];
-
-		if(!largeImage) largeImage = {
-			icon: 'text',
-			text: path.extname(currEditor)
-		};
-
-		//Deep copying
-		largeImage = {
-			icon: largeImage.icon,
-			text: largeImage.text
-		};
-
-		largeImage.text = getTranslation('type-description', {
-			type: largeImage.text
-		});
-	} else {
-		largeImage = {
-			icon: 'text',
-			text: getTranslation('developer-idle')
-		};
+		this.onlineRenderers = {};
+		this.rpc = null;
+		this.destroied = false;
 	}
-});
 
-ipcMain.on('atom-discord.config-update', (event, {i18n, privacy, behaviour: _behaviour}) => {
-	translations = i18n;
-	privacySettings = privacy;
-	behaviour = _behaviour
-});
+	setOnline(id) {
+		if(this.onlineRenderers[id]) return;
+
+		let sendAfter = !this.isRendererOnline;
+		console.log(`Editor ${id} became online.`);
+
+		this.onlineRenderers[id] = true;
+
+		if(sendAfter){
+			console.log(`New editor confirmed, sending activities...`);
+			this.sendActivity();
+		}
+	}
+
+	async setOffline(id) {
+		if(!this.onlineRenderers[id]) return;
+
+		console.log(`Editor ${id} became offline.`);
+
+		this.onlineRenderers[id] = false;
+
+		if(!this.isRendererOnline) {
+			console.log(`No editor remained, deleting activities...`);
+
+			/*setTimeout(() => {
+				// After grace period(5s), if there are no editors, destroy RPC.
+				if(!this.isRendererOnline) this.destroyRpc();
+			}, 5000);*/
+
+			// Just immediately destroying because the speed of closing atom is faster than 5s.
+			await this.destroyRpc();
+		}
+	}
+
+	setupRpc() {
+		if(this.rpc) return;
+
+		return new Promise((resolve, reject) => {
+			if(typeof Client === 'undefined') return reject("No client available!");
+
+			const rpc = new Client({ transport: 'ipc' });
+			rpc.on('ready', () => {
+				this.rpc = rpc;
+				this.destroied = false;
+				resolve();
+			});
+			rpc.login(DISCORD_ID).catch(reject);
+		});
+	}
+
+	async destroyRpc() {
+		if(this.destroied) return;
+
+		console.log("Destroying RPC Client...");
+		this.destroied = true;
+
+		const _rpc = this.rpc;
+		this.rpc = null;
+
+		await _rpc.destroy()
+		console.log("Done destroying RPC Client. It is now safe to turn off the computer.")
+	}
+
+	sendActivity() {
+		if(!this.rpc) return;
+		if(!this.isRendererOnline) return;
+
+		// Set packet variables
+		let state = this.projectName ? config.getTranslation('working-project', {
+			projectName: this.projectName
+		}) : config.getTranslation('working-no-project');
+
+		let details = this.fileName ? config.getTranslation('editing-file', {
+			fileName: this.fileName
+		}) : config.getTranslation('editing-idle');
+
+		let largeImageKey = this.largeImage ? this.largeImage.icon : null;
+		let largeImageText = this.largeImage ? this.largeImage.text : null;
+
+		let smallImageKey = config.behaviour.alternativeIcon;
+		let smallImageText = config.getTranslation('atom-description');
+
+		let startTimestamp = this.startTimestamp;
+
+		// Remove privacy-related things
+		if(!config.privacy.sendProject) state = config.getTranslation('working-no-project')
+		if(!config.privacy.sendFilename) details = config.getTranslation('editing-idle')
+		if(!config.privacy.sendFileType) largeImageKey = 'text', largeImageText = config.getTranslation('type-unknown');
+		if(!config.privacy.sendElapsed) startTimestamp = null;
+
+		// Remove small icon
+		if(!config.behaviour.sendSmallImage) smallImageKey = null, smallImageText = null;
+		if(!config.behaviour.sendLargeImage) largeImageKey = null, largeImageText = null;
+
+		let packet = normalize({
+			state,
+			details,
+
+			largeImageKey,
+			largeImageText,
+
+			smallImageKey,
+			smallImageText,
+
+			startTimestamp
+		});
+
+		this.rpc.setActivity(packet);
+	}
+
+	updateActivity(projectName, fileName) {
+		this.projectName = projectName;
+		this.fileName = fileName;
+
+		if(this.fileName && config.icon) {
+			let found = false;
+
+			config.icon.forEach((value, test) => {
+				if(found) return;
+				let result = false;
+
+				if(typeof test === 'string') result = this.fileName.endsWith(test);
+				else if(test.test) result = test.test(this.fileName);
+
+				if(result) {
+					found = true;
+					this.largeImage = value;
+				}
+			});
+
+			if(!found) this.largeImage = {
+				icon: 'text',
+				text: path.extname(this.fileName)
+			};
+
+			this.largeImage = {
+				icon: this.largeImage.icon,
+				text: config.getTranslation('type-description', {
+					type: this.largeImage.text
+				})
+			};
+
+		} else {
+			this.largeImage = {
+				icon: 'text',
+				text: config.getTranslation('developer-idle')
+			};
+		}
+	}
+
+
+	async loop() {
+		try {
+			if(this.destroied === false) {
+				await this.setupRpc(Client)
+			}
+		} catch(e) {
+			console.log(e);
+		}
+
+		if(this.isRendererOnline) this.sendActivity();
+
+		setTimeout(this.loopFunction, config.behaviour.updateTick);
+	}
+
+	get loopFunction() {
+		return this.loop.bind(this);
+	}
+
+	get isRendererOnline() {
+		return Object.keys(this.onlineRenderers).some((v) => this.onlineRenderers[v]);
+	}
+}
+
+const sender = new DiscordSender();
 
 ipcMain.on('atom-discord.discord-setup', (event, arg) => {
-	const {Client} = require(arg.rpcPath);
-	setupRpc(Client);
+	if(sender.rpc) return;
 
-	rpc.login(arg.discordId).catch(console.error);
+	const REGEX_REGEX = /^\/(.*)\/([mgiy]+)$/;
 
-	matchData = arg.matchData;
-	matchKeys = Object.keys(matchData);
+	config.icon = new Map(Object.keys(arg.matchData).map((key) => {
+		let match = key.match(REGEX_REGEX);
+		if(!match) return [key, arg.matchData[key]];
 
-	console.log("Set up discord RPC.");
+		return [new RegExp(match[1], match[2]), arg.matchData[key]];
+	}));
+
+	Client = require(arg.rpcPath).Client;
+
+	(async () => {
+		await sender.setupRpc();
+		await sender.loop();
+		console.log("Set up discord RPC.");
+	})();
+});
+
+ipcMain.on('atom-discord.config-update', (event, {i18n, privacy: _privacy, behaviour: _behaviour}) => {
+	config.translations = i18n;
+	config.behaviour = _behaviour;
+	config.privacy = _privacy;
+});
+
+ipcMain.on('atom-discord.data-update', (event, {projectName, currEditor: fileName}) => {
+	sender.updateActivity(projectName, fileName);
+});
+
+ipcMain.on('atom-discord.online', (event, {id}) => {
+	sender.setOnline(id);
+});
+
+ipcMain.on('atom-discord.offline', (event, {id}) => {
+	sender.setOffline(id);
 });
